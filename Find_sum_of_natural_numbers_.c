08e1156f121cc97718f02c4ad44ee8f97f1e9bb4
Raviteja <97425841+Ravitejadevarapu98@users.noreply.github.com>
2022-02-14 19:53:56 +0530

    #include<stdio.h> int main() {     int k,l,sum=0;     scanf("%d",&l);     for(k=1;k<=l;k++)     {         sum=sum+k;
        }
        printf("%d",sum);
        return (0); }

---
+++ Find_sum_of_natural_numbers_.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{ 
+    int k,l,sum=0; 
+    scanf("%d",&l);
+    for(k=1;k<=l;k++)
+    {
+        sum=sum+k; 
+        
+    }
+    printf("%d",sum);
+    return (0); }
+
